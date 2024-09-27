816715ddc315f1527de573f5debc863b3c8ade35
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-09-27 13:52:33 +0300

    Finish auth and flash messages add var_dumper composer package

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -17,8 +17,9 @@ class AuthController
             $user->email = $_POST['email'];
             $user->password = password_hash($_POST['password'], PASSWORD_BCRYPT);
             $user->save();
-            redirect('/');
+            redirect('/login');
         } else {
+            $_SESSION['newerror'] = "account exists or passwords don't match";
             redirect('/register');
         }
        
@@ -26,14 +27,23 @@ class AuthController
     }
 
     public function loginForm(){
-
+        view('auth/login');
     }
 
     public function login(){
-        
+        $user = User::where('email', $_POST['email']);
+        $user = $user[0] ?? null;
+        if($user && password_verify($_POST['password'], $user->password)){
+            $_SESSION['userId'] = $user->id;
+            redirect('/');
+        } else {
+            $_SESSION['newerror'] = "Credentials don't match";
+            redirect('/login');
+        }
     }
 
     public function logout(){
-        
+        unset($_SESSION['userId']);
+        redirect('/');
     }
 }
